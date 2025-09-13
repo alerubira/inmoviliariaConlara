86fd22bb977e0d1feb5cc0dc339bf925bfd9b7be
alerubira <100098255+alerubira@users.noreply.github.com>
2025-09-13 12:07:23 -0300

    Update Program.cs
    configurado para autenticacion y autoeizacion

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 var builder = WebApplication.CreateBuilder(args);
 
 // 🔹 Registrar todos los servicios primero
@@ -5,6 +6,20 @@ builder.Services.AddControllersWithViews()
        .AddSessionStateTempDataProvider(); // TempData via Session
 
 builder.Services.AddSession(); // Habilita Session
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>//el sitio web valida con cookie
+    {
+        options.LoginPath = "/Usuarios/Login";
+        options.LogoutPath = "/Usuarios/Logout";
+        options.AccessDeniedPath = "/Home/Restringido";
+        //options.ExpireTimeSpan = TimeSpan.FromMinutes(5);//Tiempo de expiración
+    });
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("Empleado", policy => policy.RequireRole("Empleado", "Administrador"));
+    options.AddPolicy("Administrador", policy => policy.RequireRole("Administrador"));
+});
+  
 
 // 🔹 Construir la app
 var app = builder.Build();
@@ -26,6 +41,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 app.UseAuthorization();
+app.UseAuthentication();
 
 // Rutas MVC
 app.MapControllerRoute(
